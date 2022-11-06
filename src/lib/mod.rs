1da42bc81d3dc124fb9fadbd69a19ae4ca25cbc2
Rafael Lúcio <rafaellucio@pm.me>
2022-11-06 20:34:21 -0300

    fix: u8 to u32 vectors and implement tests to assert its conversions

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,9 +1,7 @@
-use core::ops::{BitOr, Shl, Shr};
-use std::ops::Range;
+extern crate core;
 
-fn main() {
-    println!("Hello, world!");
-}
+use core::ops::{BitOr, Shl, Shr};
+use std::ops::{Deref, Range};
 
 const T_0_15: u32 = 0x5a827999;
 const T_16_19: u32 = T_0_15;
@@ -37,7 +35,7 @@ fn get_dst_range<T>(src: &[T], dest: &[u32], dest_offset: usize) -> Range<usize>
 
     Range {
         start: dst_start,
-        end: dst_end
+        end: dst_end,
     }
 }
 
@@ -45,13 +43,13 @@ fn get_src_range<T>(src: &[T], src_slice_len: usize) -> Range<usize> {
     if src_slice_len > src.len() {
         return Range {
             start: 0,
-            end: src.len()
-        }
+            end: src.len(),
+        };
     }
 
     Range {
         start: 0,
-        end: (src_slice_len & (src.len() - 1)) % src.len()
+        end: (src_slice_len & (src.len() - 1)) % src.len(),
     }
 }
 
@@ -64,51 +62,60 @@ pub trait Sha1 {
 pub struct ShaContext {
     size: usize,
     h: [u32; 5],
-    w: [u32; 16]
+    w: [u32; 16],
 }
 
 trait MemoryCopy<S, D> {
     /// Copies n bytes from src to memory dest, using a reference receiving point in dest
-    fn mem_cpy(src: &[S], dest: &mut [D], src_slice_len: usize, dest_offset:usize);
+    fn mem_cpy(dest: &mut [D], src: &[S], src_slice_len: usize, dest_offset: usize);
 }
 
 trait ShaSource<T> {
     fn src(i: usize, v: &[T]) -> u32;
-    fn t_0_15(t: u8, block: &[T], a: u32, b: &mut u32, c: u32, d: u32, e: &mut u32, array: &mut [u32]);
+    fn t_0_15(
+        t: u8,
+        block: &[T],
+        a: u32,
+        b: &mut u32,
+        c: u32,
+        d: u32,
+        e: &mut u32,
+        array: &mut [u32],
+    );
     fn block(h: &mut [u32; 5], block: &[T]);
     fn process(&mut self, data_in: &[T], len: usize);
 }
 
 impl MemoryCopy<u8, u8> for ShaContext {
-    fn mem_cpy(src: &[u8], dest: &mut [u8], src_slice_len: usize, dest_offset: usize) {
+    fn mem_cpy(dest: &mut [u8], src: &[u8], src_slice_len: usize, dest_offset: usize) {
         dest[dest_offset..].clone_from_slice(&src[..src_slice_len])
     }
 }
 
 impl MemoryCopy<u8, u32> for ShaContext {
-    fn mem_cpy(src: &[u8], dest: &mut [u32], src_slice_len: usize, mut dest_offset: usize) {
-        // TODO: this impl considers src and src_len larger than 4 bytes. Implement flows for lesser cases
-        let src_range = get_src_range(src, src_slice_len);
-        // TODO: Problema atual: indices até 4 quando fim do vetor pode ser até 1
-        let u32_src = &src[src_range].chunks(4).map(|c| {
-            match c.len() {
-                4 => (c[0] as u32) | ((c[1] as u32) << 8) | ((c[2] as u32) << 16) | ((c[3] as u32) << 24),
+    fn mem_cpy(dest: &mut [u32], src: &[u8], src_slice_len: usize, dest_offset: usize) {
+        let u32_src = src
+            .chunks(4)
+            .map(|c| match c.len() {
+                4 => {
+                    (c[0] as u32)
+                        | ((c[1] as u32) << 8)
+                        | ((c[2] as u32) << 16)
+                        | ((c[3] as u32) << 24)
+                }
                 3 => (c[0] as u32) | ((c[1] as u32) << 8) | ((c[2] as u32) << 16),
                 2 => (c[0] as u32) | ((c[1] as u32) << 8),
                 1 => c[0] as u32,
-                _ => panic!("")
-            }
-        }).collect::<Vec<u32>>();
+                _ => panic!(""),
+            })
+            .collect::<Vec<u32>>();
 
-        let mut dst_range = get_dst_range(src, dest, dest_offset);
-        dst_range.start = (dst_range.start / 4) + (dst_range.start % 4);
-        dst_range.end = dst_range.start + u32_src.len();
-        &dest[dst_range].clone_from_slice(&u32_src);
+        dest[..u32_src.len()].clone_from_slice(&u32_src);
     }
 }
 
 impl MemoryCopy<u32, u32> for ShaContext {
-    fn mem_cpy(src: &[u32], dest: &mut [u32], src_slice_len: usize, dest_offset: usize) {
+    fn mem_cpy(dest: &mut [u32], src: &[u32], src_slice_len: usize, dest_offset: usize) {
         // TODO: Validar se intervalo dos vetor receptor é igual ou maio que do vetor fonte
         let src_range = get_src_range(src, src_slice_len);
         let dst_range = get_dst_range(src, dest, dest_offset);
@@ -127,7 +134,16 @@ impl ShaSource<u8> for ShaContext {
             | (v[s + 3] as u32)
     }
 
-    fn t_0_15(t: u8, block: &[u8], a: u32, b: &mut u32, c: u32, d: u32, e: &mut u32, array: &mut [u32])  {
+    fn t_0_15(
+        t: u8,
+        block: &[u8],
+        a: u32,
+        b: &mut u32,
+        c: u32,
+        d: u32,
+        e: &mut u32,
+        array: &mut [u32],
+    ) {
         let temp = Self::src(t as usize, block);
         Self::set_w(t as usize, temp, array);
         *e += temp + rotate_left(a, 5) + Self::f1(*b, c, d) + T_0_15;
@@ -154,12 +170,12 @@ impl ShaSource<u8> for ShaContext {
         Self::t_0_15(7, block, d, &mut e, a, b, &mut c, &mut array);
         Self::t_0_15(8, block, c, &mut d, e, a, &mut b, &mut array);
         Self::t_0_15(9, block, b, &mut c, d, e, &mut a, &mut array);
-        Self::t_0_15(10, block, a, &mut  b, c, d, &mut  e, &mut array);
-        Self::t_0_15(11, block, e, &mut  a, b, c, &mut  d, &mut array);
-        Self::t_0_15(12, block, d, &mut  e, a, b, &mut  c, &mut array);
-        Self::t_0_15(13, block, c, &mut  d, e, a, &mut  b, &mut array);
-        Self::t_0_15(14, block, b, &mut  c, d, e, &mut  a, &mut array);
-        Self::t_0_15(15, block, a, &mut  b, c, d, &mut  e, &mut array);
+        Self::t_0_15(10, block, a, &mut b, c, d, &mut e, &mut array);
+        Self::t_0_15(11, block, e, &mut a, b, c, &mut d, &mut array);
+        Self::t_0_15(12, block, d, &mut e, a, b, &mut c, &mut array);
+        Self::t_0_15(13, block, c, &mut d, e, a, &mut b, &mut array);
+        Self::t_0_15(14, block, b, &mut c, d, e, &mut a, &mut array);
+        Self::t_0_15(15, block, a, &mut b, c, d, &mut e, &mut array);
 
         /* Round 1 - tail. Input from 512-bit mixing array */
         Self::t_16_19(16, &mut array, e, &mut a, b, c, &mut d);
@@ -251,7 +267,7 @@ impl ShaSource<u8> for ShaContext {
                 left = len;
             }
 
-            Self::mem_cpy(data_in, &mut self.w, left, len_w);
+            Self::mem_cpy(&mut self.w[(len_w & 15)..], &data_in[..len], left, len_w);
 
             len_w = (len_w + left) & 63;
             len -= left;
@@ -271,7 +287,7 @@ impl ShaSource<u8> for ShaContext {
         }
 
         if len > 0 {
-            Self::mem_cpy(data_in, &mut self.w, len, 0);
+            Self::mem_cpy(&mut self.w[0..], &data_in[..len], len, 0);
         }
     }
 }
@@ -281,10 +297,22 @@ impl ShaSource<u32> for ShaContext {
         v[i].to_be()
     }
 
-    fn t_0_15(t: u8, block: &[u32], a: u32, b: &mut u32, c: u32, d: u32, e: &mut u32, array: &mut [u32]) {
+    fn t_0_15(
+        t: u8,
+        block: &[u32],
+        a: u32,
+        b: &mut u32,
+        c: u32,
+        d: u32,
+        e: &mut u32,
+        array: &mut [u32],
+    ) {
         let temp = Self::src(t as usize, block);
         Self::set_w(t as usize, temp, array);
-        *e = (*e).wrapping_add(temp.wrapping_add(rotate_left(a, 5).wrapping_add(Self::f1(*b, c, d).wrapping_add(T_0_15))));
+        *e =
+            (*e).wrapping_add(temp.wrapping_add(
+                rotate_left(a, 5).wrapping_add(Self::f1(*b, c, d).wrapping_add(T_0_15)),
+            ));
         *b = rotate_right(*b, 2);
     }
 
@@ -308,12 +336,12 @@ impl ShaSource<u32> for ShaContext {
         Self::t_0_15(7, block, d, &mut e, a, b, &mut c, &mut array);
         Self::t_0_15(8, block, c, &mut d, e, a, &mut b, &mut array);
         Self::t_0_15(9, block, b, &mut c, d, e, &mut a, &mut array);
-        Self::t_0_15(10, block, a,&mut  b, c, d,&mut  e, &mut array);
-        Self::t_0_15(11, block, e,&mut  a, b, c,&mut  d, &mut array);
-        Self::t_0_15(12, block, d,&mut  e, a, b,&mut  c, &mut array);
-        Self::t_0_15(13, block, c,&mut  d, e, a,&mut  b, &mut array);
-        Self::t_0_15(14, block, b,&mut  c, d, e,&mut  a, &mut array);
-        Self::t_0_15(15, block, a,&mut  b, c, d,&mut  e, &mut array);
+        Self::t_0_15(10, block, a, &mut b, c, d, &mut e, &mut array);
+        Self::t_0_15(11, block, e, &mut a, b, c, &mut d, &mut array);
+        Self::t_0_15(12, block, d, &mut e, a, b, &mut c, &mut array);
+        Self::t_0_15(13, block, c, &mut d, e, a, &mut b, &mut array);
+        Self::t_0_15(14, block, b, &mut c, d, e, &mut a, &mut array);
+        Self::t_0_15(15, block, a, &mut b, c, d, &mut e, &mut array);
 
         /* Round 1 - tail. Input from 512-bit mixing array */
         Self::t_16_19(16, &mut array, e, &mut a, b, c, &mut d);
@@ -405,7 +433,7 @@ impl ShaSource<u32> for ShaContext {
                 left = len;
             }
 
-            Self::mem_cpy(data_in, &mut self.w, left, len_w);
+            Self::mem_cpy(&mut self.w, data_in, left, len_w);
 
             len_w = (len_w + left) & 63;
             len -= left;
@@ -425,12 +453,26 @@ impl ShaSource<u32> for ShaContext {
         }
 
         if len > 0 {
-            Self::mem_cpy(data_in, &mut self.w, len, 0);
+            Self::mem_cpy(&mut self.w, data_in, len, 0);
         }
     }
 }
 
 impl ShaContext {
+    fn to_byte_slice(&self) -> [u8; 20] {
+        // Use flatten once it stabilizes
+        let mut hash_out: [u8; 20] = [0; 20];
+        self.h.iter().zip((0..5).into_iter())
+            .for_each(|(constant, k_index)| {
+                hash_out[0 + (k_index * 4)] = constant.shr(24) as u8;
+                hash_out[1 + (k_index * 4)] = constant.shr(16) as u8;
+                hash_out[2 + (k_index * 4)] = constant.shr(8) as u8;
+                hash_out[3 + (k_index * 4)] = *constant as u8;
+            });
+
+        hash_out
+    }
+
     fn set_w(i: usize, val: u32, array: &mut [u32]) {
         array[i & 15] = val;
     }
@@ -460,10 +502,6 @@ impl ShaContext {
         Self::f2(b, c, d)
     }
 
-    fn zero_first_twenty_four_bits(h: &u32) -> u8 {
-        ((*h << 24) >> 24) as u8
-    }
-
     fn round(
         t: u8,
         block: &mut [u32],
@@ -477,22 +515,14 @@ impl ShaContext {
     ) {
         let temp = Self::mix(t as usize, block);
         Self::set_w(t as usize, temp, block);
-        *e = (*e).wrapping_add(temp.wrapping_add(rotate_left(a, 5).wrapping_add(f_n.wrapping_add(constant))));
+        *e = (*e).wrapping_add(
+            temp.wrapping_add(rotate_left(a, 5).wrapping_add(f_n.wrapping_add(constant))),
+        );
         *b = rotate_right(*b, 2);
     }
 
     fn t_16_19(t: u8, shamble_arr: &mut [u32], a: u32, b: &mut u32, c: u32, d: u32, e: &mut u32) {
-        Self::round(
-            t,
-            shamble_arr,
-            Self::f1(*b, c, d),
-            T_16_19,
-            a,
-            b,
-            c,
-            d,
-            e,
-        )
+        Self::round(t, shamble_arr, Self::f1(*b, c, d), T_16_19, a, b, c, d, e)
     }
 
     fn t_20_39(t: u8, shamble_arr: &mut [u32], a: u32, mut b: u32, c: u32, d: u32, mut e: u32) {
@@ -543,7 +573,7 @@ impl Sha1 for ShaContext {
         let mut ctx = Self {
             size: 0,
             h: [0; 5],
-            w: [0; 16]
+            w: [0; 16],
         };
         /* Initialize H with the magic constants (see FIPS180 for constants) */
         ctx.h[0] = 0x67452301;
@@ -561,48 +591,69 @@ impl Sha1 for ShaContext {
 
     fn finalize(&mut self) -> [u8; 20] {
         let mut pad: [u8; 64] = [0; 64];
-        let mut pad_len: [u32; 2]= [0; 2];
+        let mut pad_len: [u32; 2] = [0; 2];
         pad[0] = 0x80;
 
         let i = self.size & 63;
         self.process(&pad, 1 + (63 & (55 - i)));
         self.process(&pad_len, 8);
 
-        let mut hash_out: [u8; 20] = [0; 20];
-
-        self.h.iter().zip((0..5).into_iter()).for_each(|(h, i)| {
-            hash_out[0 + (i * 4)] = (*h >> 24) as u8;
-            hash_out[1 + (i * 4)] = (*h >> 16) as u8;
-            hash_out[2 + (i * 4)] = (*h >> 8) as u8;
-            hash_out[3 + (i * 4)] = Self::zero_first_twenty_four_bits(h);
-        });
-
-        return hash_out;
+        self.to_byte_slice()
     }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::{rotate_left, rotate_right, Sha1, ShaContext};
+    use std::cmp::max;
+    use crate::{rotate_left, rotate_right, MemoryCopy, Sha1, ShaContext};
+    use std::fmt::Binary;
+    use std::ops::{Shl, Shr};
 
     #[test]
-    fn custom_right_bit_rotation_should_return_same_as_standard_impl() {
+    fn fips180_rotate_right_and_left_are_consistent_with_core_rotate_methods() {
         let x: u32 = 5;
         let y: u32 = 2;
+
+        let std_rotate_left = x.rotate_left(y);
+        let cus_rotate_left = rotate_left(x, y);
         let std_rotate_right = x.rotate_right(y);
         let cus_rotate_right = rotate_right(x, y);
 
+        assert_eq!(std_rotate_left, cus_rotate_left);
         assert_eq!(std_rotate_right, cus_rotate_right);
     }
 
     #[test]
-    fn custom_left_bit_rotation_should_return_same_as_standard_impl() {
-        let x: u32 = 5;
-        let y: u32 = 2;
-        let std_rotate_left = x.rotate_left(y);
-        let cus_rotate_left = rotate_left(x, y);
+    fn first_w_bit_word_in_fips180_documentation_conversion_test() {
+        let w32_hex_str: u32 = 0xa103fe23;
+        let four_w32_bit_str = [
+            "1010", "0001", "0000", "0011", "1111", "1110", "0010", "0011",
+        ];
+        let w64_hex_str: u64 = 0xa103fe2332ef301a;
+        let four_w64_bit_str = [
+            "1010", "0001", "0000", "0011", "1111", "1110", "0010", "0011", "0011", "0010", "1110",
+            "1111", "0011", "0000", "0001", "1010",
+        ];
 
-        assert_eq!(std_rotate_left, cus_rotate_left);
+        let w32_ones_count = w32_hex_str.count_ones();
+        let w32_zeros_count = w32_hex_str.count_zeros();
+        let w32_bits_count = w32_ones_count + w32_zeros_count;
+        let w32_binary_representation = binary_representation(w32_hex_str);
+
+        let w64_ones_count = w64_hex_str.count_ones();
+        let w64_zeros_count = w64_hex_str.count_zeros();
+        let w64_bits_count = w64_ones_count + w64_zeros_count;
+        let w64_binary_representation = binary_representation(w64_hex_str);
+
+        assert_eq!(w32_ones_count, 15);
+        assert_eq!(w32_zeros_count, 17);
+        assert_eq!(w32_bits_count, u32::BITS);
+        assert_eq!(w32_binary_representation, four_w32_bit_str);
+
+        assert_eq!(w64_ones_count, w32_ones_count * 2);
+        assert_eq!(w64_zeros_count, w32_zeros_count * 2);
+        assert_eq!(w64_bits_count, u64::BITS);
+        assert_eq!(w64_binary_representation, four_w64_bit_str);
     }
 
     #[test]
@@ -611,8 +662,71 @@ mod test {
         let mut ctx = ShaContext::init();
         ctx.update(PHRASE.as_bytes(), PHRASE.len());
         let hash = ctx.finalize();
-        let hash_hex_str = hash.iter().map(|b| format!("{:02x}", b)).collect::<String>();
+        let hash_hex_str = hash
+            .iter()
+            .map(|b| format!("{:02x}", b))
+            .collect::<String>();
 
         assert_eq!(hash_hex_str, "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12");
+    }
+
+    #[test]
+    fn copy_a_u8_vector_into_a_u32_vector() {
+        let char_vec = ['a', 'b', 'c', 'd', 'e'];
+        let mut dest: [u32; 5] = [0; 5];
+
+        ShaContext::mem_cpy(
+            &mut dest[0..],
+            &char_vec.into_iter().collect::<String>().as_bytes()[..5],
+            5,
+            0,
+        );
+
+        assert_eq!(
+            dest,
+            [1684234849, 101, 0, 0, 0],
+            "Asserts characters bytes were correctly copied into u32 integers"
+        );
+
+        println!();
+    }
+
+    #[test]
+    fn bit_shift_method_vs_explicit_operation() {
+        let unsigned_integer: u32 = 1684234849;
+
+        assert_eq!(unsigned_integer >> 24, unsigned_integer.shr(24));
+        assert_eq!(unsigned_integer >> 16, unsigned_integer.shr(16));
+        assert_eq!(unsigned_integer >> 8, unsigned_integer.shr(8));
+        assert_eq!(unsigned_integer >> 0, unsigned_integer.shr(0));
+
+        assert_eq!(unsigned_integer << 24, unsigned_integer.shl(24));
+        assert_eq!(unsigned_integer << 16, unsigned_integer.shl(16));
+        assert_eq!(unsigned_integer << 8, unsigned_integer.shl(8));
+        assert_eq!(unsigned_integer << 0, unsigned_integer.shl(0));
+
+        let max_u32_unsigned_integer: u32 = u32::MAX;
+        assert_eq!(max_u32_unsigned_integer.shr(8), 16_777_215u32, "Assert equality for u24::MAX");
+        assert_eq!(max_u32_unsigned_integer.shr(16), 65_535u32, "Assert equality for u16::MAX");
+        assert_eq!(max_u32_unsigned_integer.shr(24), 255_u32, "Assert equality for u8::MAX");
+
+        // Assert cast has logic for pointing the least significant bits, by the amount of the new
+        // type size. It seems to make use of Copy, because it looses original information in case a cast
+        // backwards to original size is immediately made
+        assert_eq!(max_u32_unsigned_integer as u8, (max_u32_unsigned_integer.shl(24) as u32).shr(24) as u8);
+        assert_eq!(max_u32_unsigned_integer as u8, u8::MAX);
+        assert_eq!(max_u32_unsigned_integer.shr(0), u32::MAX);
+        assert_eq!((max_u32_unsigned_integer as u8) as u32, u8::MAX as u32);
+    }
+
+
+
+    fn binary_representation(x: impl Copy + Binary) -> Vec<String> {
+        format!("{:b}", x)
+            .chars()
+            .collect::<Vec<char>>()
+            .chunks(4)
+            .map(|u4_bin_str| u4_bin_str.iter().collect())
+            .collect::<Vec<String>>()
     }
 }
