df80f53b0d41b92082a6812780321fbc111ef296
Rafael Lúcio <rafaellucio@pm.me>
2023-05-28 22:39:51 -0300

    fix: incorrect doc comment on `Shake256Hasher`

--- wks/hash_functions/rs_shake256/src/shake256hasher.rs
+++ wks/hash_functions/rs_shake256/src/shake256hasher.rs
@@ -4,46 +4,44 @@ use hash_ctx_lib::{ByteArrayWrapper, GenericHasher, HasherContext};
 use internal_hasher::HashAlgorithm;
 use internal_state::ExtendedOutputFunction;
 
-/// `Shake256State` represents the state of a SHAKE128 hashing process.
+/// `Shake256Hasher` is a type that provides the SHAKE256 hashing algorithm in Rust.
 ///
-/// It holds intermediate hash calculations. However, it's important to note that starting a hashing
-/// process from an arbitrary `Shake256State` is not equivalent to resuming the original process that
-/// produced that state. Instead, it begins a new hashing process with a different set of initial values.
+/// Within the realm of cryptographic hashing, a "Hasher" designates the object that oversees the transformation of input
+/// data into a variable-size sequence of bytes. The Hasher's responsibilities include preserving the internal state of the
+/// hashing process and offering methods to both append more data and obtain the resulting hash.
 ///
-/// Therefore, a `Shake256State` extracted from a `Shake256Hasher` should not be used with the expectation
-/// of continuing the hashing operation from where it left off in the original `Shake256Hasher`. It is
-/// a snapshot of a particular point in the process, not a means to resume the process.
+/// `Shake256Hasher` complies with Rust's `Hasher` trait. This trait compliance enables its interchangeable use with other Rust hashers.
+/// It can be leveraged in any context where a type implementing `Hasher` is needed.
 ///
-/// # Example
+/// ## Examples
 ///
-/// This example demonstrates how to persist the state of a SHAKE128 hash operation:
+/// The following examples show how to use `Shake256Hasher` with both `Hash` and `Hasher`, and demonstrate the difference
+/// between the two:
 ///
-/// ```rust
+///```rust
 /// # use std::hash::{BuildHasher, Hash, Hasher};
-/// # use rs_shake256::{Shake256Hasher, Shake256State};
-/// let hello = b"hello";
-/// let world = b" world";
-/// let default_shake256state = Shake256State::<20>::default();
+/// # use rs_shake256::Shake256State;
+/// let data = b"hello";
 ///
-/// let mut default_shake256hasher = default_shake256state.build_hasher();
-/// default_shake256hasher.write(hello);
+/// // Using Hash
+/// let mut shake256hasher = Shake256State::<20>::default().build_hasher();
+/// data.hash(&mut shake256hasher);
+/// let result_via_hash = shake256hasher.finish();
 ///
-/// let intermediate_state: Shake256State<20> = default_shake256hasher.clone().into();
+/// // Using Hasher
+/// let mut shake256hasher = Shake256State::<20>::default().build_hasher();
+/// shake256hasher.write(data);
+/// let result_via_hasher = shake256hasher.finish();
 ///
-/// default_shake256hasher.write(world);
+/// // Simulating the Hash inners
+/// let mut shake256hasher = Shake256State::<20>::default().build_hasher();
+/// shake256hasher.write_usize(data.len());
+/// shake256hasher.write(data);
+/// let simulated_hash_result = shake256hasher.finish();
 ///
-/// let mut from_shake256state: Shake256Hasher<20> = intermediate_state.into();
-/// from_shake256state.write(world);
-///
-/// let default_hello_world_result = default_shake256hasher.finish();
-/// let from_arbitrary_state_result = from_shake256state.finish();
-/// assert_ne!(default_hello_world_result, from_arbitrary_state_result);
-/// ```
-///
-/// ## Note
-/// In this example, even though the internal states are the same between `default_shake128hasher` and `from_shake128state`
-/// before the `Hasher::finish_xof` call, the results are different due to `from_shake128state` being instantiated with an empty
-/// pad while the `default_shake128hasher`'s pad is already populated with `b"hello"`.
+/// assert_ne!(result_via_hash, result_via_hasher);
+/// assert_eq!(result_via_hash, simulated_hash_result);
+///```
 #[derive(Clone, Debug, Default, Eq, Hash, PartialEq)]
 pub struct Shake256Hasher<const OUTPUT_SIZE: usize>(GenericHasher<Shake256State<OUTPUT_SIZE>, OUTPUT_SIZE>);
 
