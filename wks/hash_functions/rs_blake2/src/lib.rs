b640d5cf6acf0c5e7dfa5ca773620fbdfe167d92
Rafael Lúcio <rafaellucio@pm.me>
2023-06-11 15:34:33 -0300

    docs: feat doc comment for work-in-progress hash function crates

--- wks/hash_functions/rs_blake2/src/lib.rs
+++ wks/hash_functions/rs_blake2/src/lib.rs
@@ -1,14 +1,15 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+//! # BLAKE2 - `rs_blake2` - Cryptographic Hash Function
+//!
+//! **Important Note: This crate is still a work in progress and is not ready for consumption.**
+//!
+//! BLAKE2 is a cryptographic hash function that is faster than MD5, SHA-1, SHA-2, and SHA-3, yet is at least as secure
+//! as the latest standard, SHA-3. Published by Aumasson, Neves, Wilcox-O'Hearn, and Winnerlein in 2012, it is used in
+//! various applications such as:
+//! - Cryptographic libraries and frameworks, for instance, libsodium.
+//! - File integrity checking and deduplication.
+//! - Secure communication protocols like Transport Layer Security (TLS), Secure Shell (SSH), Internet Protocol Security
+//! (IPSec), etc.
+//!
+//! This crate implements BLAKE2 as part of the [RustyShield](https://docs.rs/rs_shield/latest/rs_shield/) project.
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#![no_std]
--- wks/hash_functions/rs_md4/src/lib.rs
+++ wks/hash_functions/rs_md4/src/lib.rs
@@ -1,14 +1,12 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+//! # MD4 - `rs_md4` - Message Digest Algorithm 4
+//!
+//! **Important Note: This crate is still a work in progress and is not ready for consumption.**
+//!
+//! MD4 is a cryptographic hash function designed by Ronald Rivest in 1990. It's considered to be broken in terms of security, but has been historically used in various applications including:
+//! - The NT LAN Manager (NTLM) security protocols in Microsoft Windows.
+//! - The File Verification feature in the BSD UNIX operating system.
+//! - Secure communication protocols like Transport Layer Security (TLS), Secure Shell (SSH), Internet Protocol Security (IPSec), etc.
+//!
+//! This crate implements MD4 as part of the [RustyShield](https://docs.rs/rs_shield/latest/rs_shield/) project.
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#![no_std]
